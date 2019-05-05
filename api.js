360eecca3fc162b91481f2e4aa0eb7413a0947d5
locpv <locpv@dyn-118-138-25-224.its.monash.edu.au>
2019-05-06 08:54:27 +1000

    Finish demo for Facebook Melbourne Hackathon

--- api.js
+++ api.js
@@ -2,27 +2,31 @@ const fs = require('fs');
 const awsSdk = require('aws-sdk');
 const path = require('path');
 const window = require('global/window');
+var ffmpeg = require('fluent-ffmpeg');
+const request = require('request');
+const axios = require('axios');
 
-awsSdk.config.update({region:'ap-southeast-2'});
+awsSdk.config.update({
+    region:'ap-southeast-2',
+    accessKeyId: "AKIA5HQHA7POZMSTVM57",
+    secretAccessKey: "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"
+});
 
 const BUCKET_NAME = "subsync";
 const INPUT_FOLDER = "subsync-input";
 const OUTPUT_FOLDER = "subsync-output";
 
-const s3 = new awsSdk.S3({
-    accessKeyId: "AKIA5HQHA7PO3NRC5ZUB",
-    secretAccessKey: "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"
-});
+const s3 = new awsSdk.S3();
 
-module.exports.uploadAudioToS3 = function(fileName) {
+function uploadAudioToS3(fileName) {
     
-    const filePath = __dirname + '/public/' + fileName;
+    const filePath = __dirname + '/public/' + fileName + ".mp3";
     console.log("File path: " + filePath);
 
     const params = {
         Bucket: BUCKET_NAME,
         Body: fs.createReadStream(filePath),
-        Key: "input-subsync/" + fileName
+        Key: "input-subsync/" + fileName + ".mp3"
     };
 
     s3.upload(params, function (err, data) {
@@ -34,7 +38,7 @@ module.exports.uploadAudioToS3 = function(fileName) {
     });
 }
 
-module.exports.getJsonScript = function(fileName) {
+function getJsonScript(fileName, res) {
     
     const params = {
         Bucket: BUCKET_NAME,
@@ -47,28 +51,87 @@ module.exports.getJsonScript = function(fileName) {
             console.log(Date.now() + " not yet");
         }
         else {
-            if (window.intervalId) {
+            if (window.intervalId){
                 clearInterval(window.intervalId);
-                intervalId = null;
+                window.intervalId = null;
             }
             const filePath = __dirname + '/public/output/' + fileName + ".json";
             console.log("Output file path: " + filePath);
-            fs.writeFile(filePath, data.Body.toString());
+            //getSrtfromJson(fileName, res);
+            fs.writeFile(filePath, data.Body.toString(), function(err) {
+                if(err) {
+                    console.log(err);
+                }
+                console.log("File saved successfully!");
+                // getSrtfromJson(fileName, res);
+                res.render('laststep', { filename: `${fileName}` });
+            });
         }
     })
+}
+
+function getSrtfromJson(userText, fileName, res) {
+    console.log("go get srt from json");
+    if (!fs.existsSync(__dirname + '/public/output/' + fileName + ".json")) return;
+    console.log("start get srt from json");
+    fs.readFile(__dirname + '/public/output/' + fileName + ".json", function(err, data) {
+        const formData = {
+            aws_script: JSON.stringify(JSON.parse(data)),
+            user_script: userText
+        };
+        console.log(formData);
+        request.post({url:'https://99ad0a04.ngrok.io/', formData: formData}, function optionalCallback(err, httpResponse, body) {
+            if (err) {
+                return console.error('upload failed:', err);
+            }
+            console.log('Upload successful!  Server responded with:');
+            const filePath = __dirname + '/public/output/' + fileName + ".srt";
+            fs.writeFileSync(filePath, body, function(err) {
+                if(err) {
+                    return console.log(err);
+                }
+            });
+            console.log("File srt saved successfully!");
+            combineVideoandScript(fileName, res);
+        });
+        console.log("here");
+    });
+    
+}
+
+function combineVideoandScript(fileName, res) {
+    ffmpeg(__dirname + '/public/' + fileName)
+            .outputOptions(
+                "-vf subtitles=" + __dirname + '/public/output/' + fileName + '.srt'
+            )
+            .on('error', function(err) {
+                console.log('Error: ' + err.message);
+            })
+            .save(__dirname + '/public/' + fileName + '_final.mp4')
+            .on('end', function() {
+                res.render('result', { filename: `${fileName}_final.mp4` });
+                console.log("Done saving");
+            });
+}
+
+module.exports = {
+    combineVideoandScript,
+    uploadAudioToS3,
+    getJsonScript,
+    getSrtfromJson
 }
 
 function test(fileName) {
     const transcribeService = new awsSdk.TranscribeService();
 
-    const recordUrl = "https://s3-ap-southeast-2.amazonaws.com/" + BUCKET_NAME + "/input-subsync/" + fileName;
+    const recordUrl = "https://s3-ap-southeast-2.amazonaws.com/" + BUCKET_NAME + "/input-subsync/" + fileName + ".mp3";
     console.log("Record URL: " + recordUrl);
     const transcriptionJobName = fileName;
 
     transcribeService.startTranscriptionJob({
         LanguageCode: "en-US",
         Media: { MediaFileUri: recordUrl },
-        MediaFormat: 'mp4',
+        MediaFormat: 'mp3',
         TranscriptionJobName: transcriptionJobName,
         OutputBucketName: "subsync",
     }, function(err, data) {
--- app.js
+++ app.js
@@ -5,11 +5,12 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var multer  = require('multer')
 var upload = multer({ dest: 'public/' });
-var { uploadAudioToS3, getJsonScript } = require('./api');
+var { uploadAudioToS3, getJsonScript, combineVideoandScript, getSrtfromJson } = require('./api');
 const window = require('global/window');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var ffmpeg = require('ffmpeg');
 
 var app = express();
 
@@ -25,13 +26,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use(express.urlencoded())
 app.post("/upload", upload.single('file'), function(req, res) {
-  console.log(req.file.filename);
-  uploadAudioToS3(req.file.filename);
-  window.intervalId = setInterval(function() {
-    getJsonScript(req.file.filename);
-  }, 10000);
-  res.send("ok");
+  var process = new ffmpeg(__dirname + '/public/' + req.file.filename);
+	process.then(function (video) {
+		video.fnExtractSoundToMP3(__dirname + '/public/' + req.file.filename + '.mp3', function (error, file) {
+			if (!error)
+        console.log('Audio file: ' + file);
+      console.log("Converted");
+      uploadAudioToS3(req.file.filename);
+      window.intervalId = setInterval(function() {
+        getJsonScript(req.file.filename, res);
+      }, 10000);
+		});
+	}, function (err) {
+		console.log('Error: ' + err);
+  });
+});
+
+app.post("/result", function(req, res) {
+  const userText = req.body.userText;
+  var filename = req.body.filename;
+  getSrtfromJson(userText, filename, res);
+});
+
+app.get('/download', function(req, res){
+  var file = __dirname + '/public/' + req.query.filename;
+  res.download(file); // Set disposition and send it.
 });
 
 // catch 404 and forward to error handler
